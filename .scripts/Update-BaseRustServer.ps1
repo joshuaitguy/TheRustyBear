713dc954c24e9f29972c5f022f0c6c610ec818f2
Joshua Irwin <joshua@irwin.ws>
2021-07-12 18:01:57 -0700

    Update

--- .scripts/Update-BaseRustServer.ps1
+++ .scripts/Update-BaseRustServer.ps1
@@ -3,4 +3,4 @@ param(
     [string]$ServerPath = "G:\Rust"
 )
 
-& $SteamCmdPath\steamcmd.exe +login anonymous +force_install_dir $ServerPath +app_update 258550 validate
+& $SteamCmdPath\steamcmd.exe +login anonymous +force_install_dir $ServerPath +app_update 258550 validate +quit
