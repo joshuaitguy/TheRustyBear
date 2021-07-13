32aee517f7cbbf4a22c8b7f97c9e6e2b9b4d7f9a
Joshua Irwin <joshua@irwin.ws>
2021-07-12 18:24:43 -0700

    fix

--- .scripts/Install-RustOxide.ps1
+++ .scripts/Install-RustOxide.ps1
@@ -2,12 +2,17 @@ param (
     [string]$ServerBasePath = "G:\Rust"
 )
 
-$tempFile = New-TemporaryFile
+$tempFile = Join-Path -Path $Env:TEMP -ChildPath "RustOxide.zip"
+
+if((Test-Path -Path $tempFile) -eq $true)
+{
+    Remove-Item -Path $tempFile -Force -Confirm:$false
+}
 
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
 
-Invoke-WebRequest -Uri "https://umod.org/games/rust/download?tag=public" -OutFile $tempFile.FullName
+Invoke-WebRequest -Uri "https://umod.org/games/rust/download?tag=public" -OutFile $tempFile 
 
-Expand-Archive -Path $tempFile.FullName -DestinationPath $ServerBasePath -Force -Confirm:$false
+Expand-Archive -Path $tempFile -DestinationPath $ServerBasePath -Force -Confirm:$false
 
-Remove-Item -Path $tempFile.FullName -Force -Confirm:$false
+Remove-Item -Path $tempFile -Force -Confirm:$false
