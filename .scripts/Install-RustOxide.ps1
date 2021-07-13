322b51f65d306f45a56ba950afbf48fbc66d5e1b
Joshua Irwin <joshua@irwin.ws>
2021-07-12 18:33:21 -0700

    removing starter remove statement

--- .scripts/Install-RustOxide.ps1
+++ .scripts/Install-RustOxide.ps1
@@ -4,11 +4,6 @@ param (
 
 $tempFile = Join-Path -Path $Env:TEMP -ChildPath "RustOxide.zip"
 
-if((Test-Path -Path $tempFile) -eq $true)
-{
-    Remove-Item -Path $tempFile -Force
-}
-
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
 
 Invoke-WebRequest -Uri "https://umod.org/games/rust/download?tag=public" -OutFile $tempFile 
