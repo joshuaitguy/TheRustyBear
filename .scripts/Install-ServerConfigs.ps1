edb4e8b93b55dc416ff5603f489575842fb6d1b6
Joshua Irwin <joshua@irwin.ws>
2021-07-13 18:02:59 -0700

    Ignoring failures to create directories

--- .scripts/Install-ServerConfigs.ps1
+++ .scripts/Install-ServerConfigs.ps1
@@ -3,9 +3,9 @@ param(
     [string]$ServerRepoPath
 )
 
-New-Item -Path $ServerPath -Name cfg -ItemType Directory | Out-Null
-New-Item -Path $ServerPath -Name oxide -ItemType Directory | Out-Null
-New-Item -Path "$ServerPath\oxide" -Name config -ItemType Directory | Out-Null
-New-Item -Path "$ServerPath\oxide" -Name plugins -ItemType Directory | Out-Null
+New-Item -Path $ServerPath -Name cfg -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
+New-Item -Path $ServerPath -Name oxide -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
+New-Item -Path "$ServerPath\oxide" -Name config -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
+New-Item -Path "$ServerPath\oxide" -Name plugins -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
 
 Get-ChildItem -Path "$ServerRepoPath\*" | Copy-Item -Destination $ServerPath -Force -Recurse -Verbose
